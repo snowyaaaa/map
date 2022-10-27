f4e4f1a6aa7c9d9e13529db7b0d39aab67dc2f21
snowyaaaa <84092173+snowyaaaa@users.noreply.github.com>
2022-10-27 14:10:21 +0400

    Update New_Map_beta_shifr.user.js

--- New_Map_beta_shifr.user.js
+++ New_Map_beta_shifr.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New_Map_beta_shifr
 // @namespace    http://tampermonkey.net/
-// @version      1.22
+// @version      1.22.1
 // @description  try to take over the world!
 // @author       Artak and Endless Night
 // @include      *://pixelplanet.fun/*
